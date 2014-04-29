6afb2c93ed74aa0cee062aeb4f712d04dd7df571
shiren1118 <shiren1118@126.com>
2014-04-29 19:36:24 +0800

    init with rails

---
+++ chat/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chat::Application.config.secret_key_base = '090041b8f99272f4021c2329679211965fbd06c3f4e0f5eced900d31863824abd6085cd6c1fa2462964c923f70aef8a73ac9ecb62ceb3838194ce6e6716e92ac'
