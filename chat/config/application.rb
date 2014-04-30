3e03b1b9b6a967de596fd1030ca312a1ee352222
shiren1118 <shiren1118@126.com>
2014-04-30 13:31:49 +0800

    init

--- chat/config/application.rb
+++ chat/config/application.rb
@@ -19,5 +19,52 @@ module Chat
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+		
+    # Activate observers that should always be running.
+    # config.active_record.observers = :business_entity_observer
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Beijing'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # I18n.enforce_available_locales = false
+		#     config.i18n.available_locales = ['zh-CN', :en]
+		#     config.i18n.default_locale = 'zh-CN'
+
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password, :access_token]
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
+
+    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    # config.active_record.schema_format = :sql
+
+    # Enforce whitelist mode for mass assignment.
+    # This will create an empty whitelist of attributes available for mass-assignment for all models
+    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
+    # parameters by using an attr_accessible or attr_protected declaration.
+    config.active_record.whitelist_attributes = false
+
+    # Enable the asset pipeline
+    config.assets.enabled = false
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '/api/*', headers: :any, methods: [:get, :post, :put, :delete]
+      end
+    end
+		
   end
 end
--- chat/config/routes.rb
+++ chat/config/routes.rb
@@ -1,4 +1,16 @@
 Chat::Application.routes.draw do
+	
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+			get 'test' => 'users#test'
+			
+      resources :users do
+				get 'index' => 'users#index'
+				
+			end
+		end
+	end
+	
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
