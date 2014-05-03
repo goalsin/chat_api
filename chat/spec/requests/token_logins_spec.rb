af95f591654683b8ad22bf17ced856b26b88e7b7
shiren1118 <shiren1118@126.com>
2014-05-03 11:19:52 +0800

    token spec

---
+++ chat/spec/requests/token_logins_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+
+describe "TokenLogins" do
+  describe "GET /api/v1/login" do
+
+		before(:each) do
+		  @user = User.create(:email=>'shiren1118@126.com',:password => "000000")
+		end
+		
+    it "works! (now write some real specs)" do
+      get '/api/v1/login?email=shiren1118@126.com&password=000000'
+			puts response.body
+			puts Rails.env
+      expect(response.status).to be(200)
+			
+			token = MultiJson.decode(response.body)['token']
+		 	expect(token.length).to eq(32)
+    end
+  end
+end
