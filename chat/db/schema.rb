3d98fa8e1fa209ce8f89be141f1cef5d7377607f
alfred sang <goalsin@126.com>
2014-05-15 00:29:18 +0000

    init

--- chat/db/schema.rb
+++ chat/db/schema.rb
@@ -41,13 +41,6 @@ ActiveRecord::Schema.define(version: 20140514030222) do
     t.datetime "updated_at"
   end
 
-  create_table "tes", force: true do |t|
-    t.text     "name"
-    t.integer  "ds"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "email"
     t.string   "password"
